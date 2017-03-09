c52318e2a029b976c98b6e1011ef247cd90ed645
singhakash409 <singh.akash409@gmail.com>
2017-03-09 17:36:26 +0530

    Correct Design Consumption Error

---
+++ Modules/Menu/Source/Module/Controllers/UpdateTableStructureController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Service;
+using AdminSetup.Models.Models;
+using ProjLib.ViewModels;
+
+namespace Presentation.Controllers
+{
+    [Authorize]
+    public class UpdateTableStructureController : Controller
+    {
+        public UpdateTableStructureController()
+        {
+        }
+
+
+
+
+
+    }
+}
